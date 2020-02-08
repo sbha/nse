01722aac9c42eb6452fc062527882f67b683e4c8
“sbha” <sbharty@gmail.com>
2020-02-08 13:40:03 -0600

    function cleanup

--- group_by_mean.R
+++ group_by_mean.R
@@ -6,13 +6,14 @@ group_by_mean <- function(d, g, v){
   
   d %>% 
     group_by(!! group_var) %>% 
-    summarise(cars = n(),
-              avg = mean(!! mean_var))
+    summarise(count = n(),
+              mean = mean(!! mean_var))
   
 }
 
 
 df <- rownames_to_column(mtcars) %>% as_tibble()
 
-
 group_by_mean(df, cyl, hp)
+
+
