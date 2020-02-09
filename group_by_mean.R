be3dd6707dee1d1e8575d619c435b4b35ca7c14b
“sbha” <sbharty@gmail.com>
2020-02-09 17:48:13 -0600

    allowing multiple grouping variables

--- group_by_mean.R
+++ group_by_mean.R
@@ -1,11 +1,12 @@
 library(tidyverse)
 
-group_by_mean <- function(d, g, v){
-  group_var <- enquo(g)
+group_by_mean <- function(d, v, ...){
   mean_var <- enquo(v)
+  group_var <- enquos(...)
+  
   
   d %>% 
-    group_by(!! group_var) %>% 
+    group_by(!!! group_var) %>% 
     summarise(count = n(),
               mean = mean(!! mean_var))
   
@@ -14,6 +15,4 @@ group_by_mean <- function(d, g, v){
 
 df <- rownames_to_column(mtcars) %>% as_tibble()
 
-group_by_mean(df, cyl, hp)
-
-
+group_by_mean(df, mpg, cyl, hp, gear)
